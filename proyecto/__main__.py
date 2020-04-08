af814d6e14abac147264f13f857d1d6bd5fbdea8
Nicolas Macian <nicoomacian@gmail.com>
2020-04-07 22:00:26 -0300

    Bug fix

--- proyecto/__main__.py
+++ proyecto/__main__.py
@@ -1,9 +1,9 @@
 import os
-from proyecto import checker
-from proyecto import checker_pi
 
 if __name__ == '__main__':
     if os.name == 'nt':
+        from proyecto import checker
         checker.run()
     else:
+        from proyecto import checker_pi
         checker_pi.run()
