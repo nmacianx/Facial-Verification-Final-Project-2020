72015a120f952df50a3f8654d7ada1af8c4c9545
Nicolas Macian <nicoomacian@gmail.com>
2020-04-21 10:25:30 -0300

    Test file added
    Simple recognition test added

--- proyecto/test.py
+++ proyecto/test.py
@@ -1,16 +1,83 @@
 import cv2 as cv
+import numpy as np
+import proyecto.data.settings as SETTINGS
+from proyecto.utils.recognition.side_functions import getOutputsNames
 
+classes = ['persona']
+
+# Draw the predicted bounding box
+def drawPred(classId, conf, left, top, right, bottom):
+    # Draw a bounding box.
+    cv.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 3)
+    
+    label = '%.2f' % conf
+        
+    # Get the label for the class name and its confidence
+    if classes:
+        assert(classId < len(classes))
+        label = '%s:%s' % (classes[classId], label)
+
+    #Display the label at the top of the bounding box
+    labelSize, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+    top = max(top, labelSize[1])
+    cv.putText(frame, label, (left, top), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))
+
+# Remove the bounding boxes with low confidence using non-maxima suppression
+def postprocess(frame, outs):
+    frameHeight = frame.shape[0]
+    frameWidth = frame.shape[1]
+
+    classIds = []
+    confidences = []
+    boxes = []
+    # Scan through all the bounding boxes output from the network and keep only the
+    # ones with high confidence scores. Assign the box's class label as the class with the highest score.
+    classIds = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > SETTINGS.recog_threshold:
+                center_x = int(detection[0] * frameWidth)
+                center_y = int(detection[1] * frameHeight)
+                width = int(detection[2] * frameWidth)
+                height = int(detection[3] * frameHeight)
+                left = int(center_x - width / 2)
+                top = int(center_y - height / 2)
+                classIds.append(classId)
+                confidences.append(float(confidence))
+                boxes.append([left, top, width, height])
+
+    # Perform non maximum suppression to eliminate redundant overlapping boxes with
+    # lower confidences.
+    indices = cv.dnn.NMSBoxes(boxes, confidences, SETTINGS.recog_threshold, SETTINGS.nms_threshold) 
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        left = box[0]
+        top = box[1]
+        width = box[2]
+        height = box[3]
+        drawPred(classIds[i], confidences[i], left, top, left + width, top + height)
+
+
+net = cv.dnn.readNetFromDarknet(SETTINGS.cfg_recog, SETTINGS.recog_weights)
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
 cap = cv.VideoCapture(0)
-
-print(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-print(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 
 while True:        
 
     hasFrame, frame = cap.read()
     frame = frame[32:448, 112:528]
-    print(frame.shape)
-    # print()
+    blob = cv.dnn.blobFromImage(frame, 1/255, (SETTINGS.inp_width, SETTINGS.inp_height), [0,0,0], 1, crop=False)
+    # Sets the input to the network and runs the forward pass to get output of the output layers
+    net.setInput(blob)    
+    outs = net.forward(getOutputsNames(net))
+    postprocess(frame, outs)
 
     key = cv.waitKey(1)
     cv.imshow('Sistema de deteccion facial', frame)
