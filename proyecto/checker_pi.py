dbe12bd20962b9c16f2acf283135d4870807358a
Nicolas Macian <nicoomacian@gmail.com>
2020-05-24 20:26:51 -0300

    Update checker_pi.py

--- proyecto/checker_pi.py
+++ proyecto/checker_pi.py
@@ -56,13 +56,13 @@ def id_checker(verification_model, database, identity):
             # Sets the input to the network and runs the forward pass to get output of the output layers
             net.setInput(blob)    
             outs = net.forward(getOutputsNames(net))
-            indices, boxes = processNetworkOutput(frame.shape[0], frame.shape[1], outs)
-            if len(indices) == 1:   # If one face was detected
-                face, position = processFaces(frame, indices, boxes)
+            boxes = processNetworkOutput(frame.shape[0], frame.shape[1], outs)
+            if len(boxes) == 1:   # If one face was detected
+                face, position = processFaces(frame, boxes)
                 face = face[0]  # Get the first face in the array (there should be only one as indices=1)
                 position = position[0]  # Get the first position element in the array (there should be only one as indices=1)
                 state, attempts = handleVerification(frame, face, position, state, attempts, identity, verification_model, database)
-            elif len(indices) > 1:  # More than one face detected
+            elif len(boxes) > 1:  # More than one face detected
                 attempts = 0
                 handleNotVerifying(frame, state, option='many')
             else:       # No face was detected
